f6ce91eef2c29e5cb1e5df0f3fcf117504d3aa8a
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-05-20 19:06:22 +0300

    Added Client model, fixed names

--- hackathon_aki/clients/models.py
+++ hackathon_aki/clients/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 
 class Client(models.Model):
-    pass
+    e_mail = models.CharField('Электронная почта', max_length=50, null=True, blank=True)
+    password = models.CharField('Пароль', max_length=50, null=True, blank=True)
+    name = models.CharField('Имя', max_length=50, null=True, blank=True)
+    surname = models.CharField('Фамилия', max_length=50, null=True, blank=True)
+    middle_name = models.CharField('Отчество', max_length=50, null=True, blank=True)
+    phone_number = models.CharField('Номер телефона', max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.surname} {self.name} {self.middle_name}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
--- hackathon_aki/main/models.py
+++ hackathon_aki/main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class File(models.Model):
-    file_name = models.CharField(db_column='Имя', max_length=50, null=True, blank=True)
-    file_data = models.BinaryField(db_column='Данные', null=True, blank=True)
+    file_name = models.CharField('Имя', max_length=50, null=True, blank=True)
+    file_data = models.BinaryField('Данные', null=True, blank=True)
 
     def __str__(self):
         return self.file_name
