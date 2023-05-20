090c0e1f81f1c7290f9c138153d859732ae59b71
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-05-20 17:02:36 +0300

    Removed cached

--- hackathon_aki/hackathon_aki/config.py
+++
@@ -1,7 +0,0 @@
-DB_NAME = ''  # Name of the database
-
-DB_HOST = ''  # Host ip of the database (for local database use 'localhost')
-DB_PORT = 0000  # Port of the database
-
-DB_USER_LOGIN = ''  # User login to connect to the database
-DB_USER_PASSWORD = ''  # User password to connect to the database
