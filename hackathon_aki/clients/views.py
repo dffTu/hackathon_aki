4df890556e124e12a713da94d5e7ae79957e1d0e
Timur Nasretdinov <trnasretdinov_1@edu.hse.ru>
2023-05-21 17:47:15 +0300

    make first simple registration form

--- hackathon_aki/clients/views.py
+++ hackathon_aki/clients/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ClientRegistrationForm
+
 
 def registration(request):
     data = {'form': ClientRegistrationForm()}
-    return render(request, 'clients/registration.html', data)
-
+    if request.method == 'POST':
+        form = ClientRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(request.path)
+        return redirect(request.path)
+    else:
+        return render(request, 'clients/registration.html', data)
