f37ad131da79c181d11ba225696bec2bddff15e9
Timur Nasretdinov <trnasretdinov_1@edu.hse.ru>
2023-05-21 19:33:46 +0300

    Added first version of catalogue.

--- hackathon_aki/hackathon_aki/urls.py
+++ hackathon_aki/hackathon_aki/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('client/', include('clients.urls')),
+    path('catalogue/', include('platforms.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
