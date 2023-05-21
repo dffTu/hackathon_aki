bb3aaf19952a291d513578d533c875c9c6b34793
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-05-21 18:54:06 +0300

    Changed client forms

--- hackathon_aki/clients/forms.py
+++ hackathon_aki/clients/forms.py
@@ -1,35 +1,42 @@
 from .models import Client
-from django.forms import ModelForm, TextInput, EmailInput
+from django import forms
 
 
-class ClientRegistrationForm(ModelForm):
+class ClientRegistrationForm(forms.ModelForm):
+    repeat_password = forms.CharField(max_length=50, widget=forms.TextInput(attrs={
+                'class': 'form-control',
+                'type': 'password',
+                'placeholder': 'Повторите пароль',
+    }))
+
+
     class Meta:
         model = Client
         fields = ['e_mail', 'password', 'name', 'surname', 'middle_name', 'phone_number']
 
         widgets = {
-            'e_mail': EmailInput(attrs={
+            'e_mail': forms.EmailInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'E-mail',
             }),
-            'password': TextInput(attrs={
+            'password': forms.TextInput(attrs={
                 'class': 'form-control',
                 'type': 'password',
                 'placeholder': 'Пароль',
             }),
-            'name': TextInput(attrs={
+            'name': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Имя',
             }),
-            'surname': TextInput(attrs={
+            'surname': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Фамилия',
             }),
-            'middle_name': TextInput(attrs={
+            'middle_name': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Отчество',
             }),
-            'phone_number': TextInput(attrs={
+            'phone_number': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Номер телефона',
             }),
