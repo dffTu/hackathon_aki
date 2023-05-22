cc9eca03b57e1b397d874e015c9ed5c2036d91ce
Timur Nasretdinov <trnasretdinov_1@edu.hse.ru>
2023-05-22 18:04:30 +0300

    Added search function.

---
+++ hackathon_aki/platforms/search_utils.py
@@ -0,0 +1,40 @@
+import re
+
+
+def levenshtein_distance(a: str, b: str) -> int:
+    n = len(a)
+    m = len(b)
+    distance = [[0] * m for i in range(n)]
+    for i in range(n):
+        distance[i][0] = i
+    for i in range(m):
+        distance[0][i] = i
+    for i in range(1, n):
+        for j in range(1, m):
+            is_different = 1
+            if a[i] == b[j]:
+                is_different = 0
+            distance[i][j] = min(distance[i][j - 1] + 1, distance[i - 1][j] + 1, distance[i - 1][j - 1] + is_different)
+    return distance[n - 1][m - 1]
+
+
+def search_platforms(search_request: str, platforms_names: list[str]) -> list[str]:
+    words_in_request = re.split(r'[ ,.;:_!+@#$|()\"\'\-\\\[\]]+', search_request)
+    levenshtein_list = []
+
+    for platform in platforms_names:
+        words_in_platform = re.split(r'[ ,.;:_!+@#$|()\"\'\-\\\[\]]+', platform)
+        levenshtein_sum = 0
+        for request_word in words_in_request:
+            best = 1000000
+            for platform_word in words_in_platform:
+                best = min(best, levenshtein_distance(platform_word, request_word))
+            levenshtein_sum += best
+        levenshtein_list.append((levenshtein_sum, platform))
+
+    levenshtein_list.sort(key=lambda x: x[0])
+    answer = []
+    for i in levenshtein_list:
+        print(i[0], i[1])
+        answer.append(i[1])
+    return answer
