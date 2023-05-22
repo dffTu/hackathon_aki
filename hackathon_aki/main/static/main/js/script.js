a86bdbf7084c61c8b48deb1015ee2bc268709c18
Timur Nasretdinov <trnasretdinov_1@edu.hse.ru>
2023-05-22 20:06:54 +0300

    Changed cringe hat.

--- hackathon_aki/main/static/main/js/script.js
+++ hackathon_aki/main/static/main/js/script.js
@@ -1,12 +1,35 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("open-user");
+var modal = document.getElementById("authorize-window");
 
-btn.onclick = function () {
+var authorize_window = document.getElementById("authorize-content");
+var register_window = document.getElementById("register-content");
+var log_in_window = document.getElementById("log-in-content");
+
+var open_user_button = document.getElementById("open-user");
+
+var register_button = document.getElementById("register");
+var log_in_button = document.getElementById("log-in");
+
+open_user_button.onclick = function () {
     modal.style.display = "block";
+    authorize_window.style.display = "flex";
+    log_in_window.style.display = "flex";
+}
+
+register_button.onclick = function () {
+    register_window.style.display = "flex";
+    log_in_window.style.display = "none";
+}
+
+log_in_button.onclick = function () {
+    log_in_window.style.display = "flex";
+    register_window.style.display = "none";
 }
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        authorize_window.style.display = "none";
+        register_window.style.display = "none";
+        log_in_window.style.display = "none";
     }
 }
