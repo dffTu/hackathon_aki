8ad6ca7d4d4601d13913290b5ce6565a02413d65
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-05-22 19:15:56 +0300

    Added attachment for platforms

--- hackathon_aki/organizers/views.py
+++ hackathon_aki/organizers/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from main.models import PlatformAttachment
+from platforms.forms import PlatformCreatingForm, PlatformFileAttachingForm
 from .forms import UserOrganizerRegistrationForm, ProfileOrganizerRegistrationForm
 
 
@@ -49,6 +51,35 @@ def registration(request):
     return render(request, 'organizers/registration.html', {'user_form': UserOrganizerRegistrationForm(),
                                                             'profile_form': ProfileOrganizerRegistrationForm(),
                                                             'errors': errors})
+
+
+def create_platform(request):
+    if not request.user.is_authenticated or not hasattr(request.user, 'organizer'):
+        return redirect('home')
+
+    errors = {'name': [],
+              'description': []}
+
+    if request.method == 'POST':
+        creating_form = PlatformCreatingForm(request.POST)
+        attachment_form = PlatformFileAttachingForm(request.POST, request.FILES)
+
+        is_valid = creating_form.validate(errors)
+        if is_valid and attachment_form.is_valid():
+            platform = creating_form.save(commit=False)
+            platform.organizer = request.user.organizer
+            platform.rating = 5
+            platform.save()
+
+            for file_description in attachment_form.cleaned_data['file_field']:
+                file = PlatformAttachment(platform=platform, file=file_description)
+                file.save()
+
+            return redirect('show_organizer_platforms')
+
+    return render(request, 'platforms/create_platform.html', {'creating_form': PlatformCreatingForm(),
+                                                              'attachment_form': PlatformFileAttachingForm(),
+                                                              'errors': errors})
 
 
 def show_organizer_profile(request):
--- hackathon_aki/platforms/urls.py
+++ hackathon_aki/platforms/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.redirect_to_first_page),
     path('page/<int:page_id>/', views.show_page, name='show_page'),
-    path('create_platform/', views.create_platform, name='create_platform'),
 ]
