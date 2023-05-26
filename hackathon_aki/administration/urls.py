8971e39aa624f7106c5be485e80c65e1aaac6c35
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-05-26 20:19:44 +0300

    Fixed administration

--- hackathon_aki/administration/urls.py
+++ hackathon_aki/administration/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.redirect_to_first_page),
-    path('page/<int:page_id>/', views.show_page, name='show_page'),
+    path('page/<int:page_id>/', views.show_page, name='show_unverified_page'),
 ]
