6a9f8522b4e6268f9d08a5cdf8eaa8dbc461c1cb
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-05-28 06:24:04 +0300

    Fixed slider

--- hackathon_aki/platforms/static/platforms/js/price_slider.js
+++ hackathon_aki/platforms/static/platforms/js/price_slider.js
@@ -1,7 +1,23 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", document_init);
+
 const rangeInput = document.querySelectorAll(".range-input input"),
 priceInput = document.querySelectorAll(".price-input input"),
 range = document.querySelector(".slider .progress");
 let priceGap = 1;
+
+function document_init() {
+    let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+
+    if ((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+        rangeInput[0].value = minPrice;
+        range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+    }
+}
 
 priceInput.forEach(input =>{
     input.addEventListener("input", e =>{
